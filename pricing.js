bc3c1ae206f0a7c7aae8a34d117d47c50e60a4c0
akashkmt <akashkmt963@gmail.com>
2022-04-16 16:16:40 +0530

    todays work

---
+++ pricing.js
@@ -0,0 +1,64 @@
+document.querySelector("#opration3").addEventListener('click',chang());
+function chang(){
+    var text = document.querySelector('#opration3').innerText
+
+    if( text == "Switch to Monthly"){
+      
+       // document.querySelector("#opration1").innerText="$39"
+       // document.querySelector("#opration2").innerText="Billed Monthaly"
+    }
+}
+function MarketingCalendar() {
+    window.location.href = "MarketingCalendar.html";
+  }
+  function MarketingSuite() {
+    window.location.href = "MarketingSuite.html";
+  }
+  function HeadlineStudio() {
+    window.location.href = "HeadlineStudio.html";
+  }
+  function ActionableMarketingInstitute() {
+    window.location.href = "ActionableMarketingInstitute.html";
+  }
+  function CustomerStories() {
+    window.location.href = "CustomerStories.html";
+  }
+  function BuildYourCaseForCoSchedule() {
+    window.location.href = "BuildYourCaseForCoSchedule.html";
+  }
+  function CoScheduleBlog() {
+    window.location.href = "CoScheduleBlog.html";
+  }
+  function CoScheduleGuide() {
+    window.location.href = "CoScheduleGuide.html";
+  }
+  function MarketingIdeasHub() {
+    window.location.href = "MarketingIdeasHub.html";
+  }
+  function HeadlineAnalyzer() {
+    window.location.href = "HeadlineAnalyzer.html";
+  }
+  function SocialMessageOptimizer() {
+    window.location.href = "SocialMessageOptimizer.html";
+  }
+  function EmailSubjectLineTester() {
+    window.location.href = "EmailSubjectLineTester.html";
+  }
+  function tenxMarketingFormula() {
+    window.location.href = "10xMarketingFormula.html";
+  }
+  function GetStartedwithAgileMarketing() {
+    window.location.href = "GetStartedwithAgileMarketing.html";
+  }
+  function AgileMarketingGuide() {
+    window.location.href = "AgileMarketingGuide.html";
+  }
+  function MarketingStrategyGuide() {
+    window.location.href = "MarketingStrategyGuide.html";
+  }
+  function ContentMarketingGuide() {
+    window.location.href = "ContentMarketingGuide.html";
+  }
+  function ActionableMarketingPodcast() {
+    window.location.href = "ActionableMarketingPodcast.html";
+  }
