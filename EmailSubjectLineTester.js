43edb01cd592f88ecba43870155cba173aedc135
akashkmt <akashkmt963@gmail.com>
2022-04-17 22:23:06 +0530

    almost done

---
+++ EmailSubjectLineTester.js
@@ -0,0 +1,57 @@
+function MarketingCalendar() {
+    window.location.href = "MarketingCalendar.html";
+  }
+  function MarketingSuite() {
+    window.location.href = "MarketingSuite.html";
+  }
+  function HeadlineStudio() {
+    window.location.href = "HeadlineStudio.html";
+  }
+  function ActionableMarketingInstitute() {
+    window.location.href = "ActionableMarketingInstitute.html";
+  }
+  function CustomerStories() {
+    window.location.href = "CustomerStories.html";
+  }
+  function BuildYourCaseForCoSchedule() {
+    window.location.href = "BuildYourCaseForCoSchedule.html";
+  }
+  function CoScheduleBlog() {
+    window.location.href = "CoScheduleBlog.html";
+  }
+  function CoScheduleGuide() {
+    window.location.href = "CoScheduleGuide.html";
+  }
+  function MarketingIdeasHub() {
+    window.location.href = "MarketingIdeasHub.html";
+  }
+  function HeadlineAnalyzer() {
+    window.location.href = "HeadlineAnalyzer.html";
+  }
+  function SocialMessageOptimizer() {
+    window.location.href = "SocialMessageOptimizer.html";
+  }
+  function EmailSubjectLineTester() {
+    window.location.href = "EmailSubjectLineTester.html";
+  }
+  function tenxMarketingFormula() {
+    window.location.href = "10xMarketingFormula.html";
+  }
+  function GetStartedwithAgileMarketing() {
+    window.location.href = "GetStartedwithAgileMarketing.html";
+  }
+  function AgileMarketingGuide() {
+    window.location.href = "AgileMarketingGuide.html";
+  }
+  function MarketingStrategyGuide() {
+    window.location.href = "MarketingStrategyGuide.html";
+  }
+  function ContentMarketingGuide() {
+    window.location.href = "ContentMarketingGuide.html";
+  }
+  function ActionableMarketingPodcast() {
+    window.location.href = "ActionableMarketingPodcast.html";
+  }
+  function GetStart(){
+      window.location.href="signIn.html";
+  }
